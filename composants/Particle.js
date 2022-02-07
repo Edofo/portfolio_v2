0c56dd0b80c7d680c52745ef2cdb37e573a37b5d
Edofo <48387251+Edofo@users.noreply.github.com>
2022-02-07 12:40:01 +0100

    Update Particle.js

--- composants/Particle.js
+++ composants/Particle.js
@@ -6,56 +6,56 @@ const Particle = () => {
             id="tsparticles"
             options={{
             
-            fpsLimit: 120,
-            interactivity: {
-                events: {
-                onClick: {
-                    enable: false,
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                    onClick: {
+                        enable: false,
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "grab",
+                    },
+                    resize: true,
+                    },
                 },
-                onHover: {
-                    enable: true,
-                    mode: "grab",
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        enable: false
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: false,
+                        speed: 0.5,
+                        straight: false,
+                    },
+                    number: {
+                    density: {
+                        enable: true,
+                        area: 1500,
+                    },
+                        value: 20,
+                    },
+                    opacity: {
+                        value: 0.2,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: true,
+                        value: 5,
+                    },
                 },
-                resize: true,
-                },
-            },
-            particles: {
-                color: {
-                    value: "#ffffff",
-                },
-                links: {
-                    enable: false
-                },
-                collisions: {
-                    enable: true,
-                },
-                move: {
-                    direction: "none",
-                    enable: true,
-                    outMode: "bounce",
-                    random: false,
-                    speed: 0.5,
-                    straight: false,
-                },
-                number: {
-                density: {
-                    enable: true,
-                    area: 1500,
-                },
-                    value: 20,
-                },
-                opacity: {
-                    value: 0.2,
-                },
-                shape: {
-                    type: "circle",
-                },
-                size: {
-                    random: true,
-                    value: 5,
-                },
-            },
-            detectRetina: true,
+                detectRetina: true,
             }}
         />
     )
