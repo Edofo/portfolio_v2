361fba3a06d75f6e8b3116b386b98994d05fadbe
Edofo <Edofo@users.noreply.github.com>
2022-02-07 16:27:40 +0100

    update

--- composants/home/Skill.js
+++ composants/home/Skill.js
@@ -1,8 +1,69 @@
+import { useEffect, useState } from 'react'
+import styles from '../../styles/home/Skill.module.scss'
+
 const Skill = () => {
+
+    const listSkill = {
+        framework: [
+            {
+                title: 'ReactJS',
+                number: '9'
+            },
+            {
+                title: 'NextJS',
+                number: '8'
+            },
+        ],
+        languages: [
+            {
+                title: 'HTML',
+                number: '10'
+            },
+            {
+                title: 'CSS/SCSS',
+                number: '10'
+            },
+            {
+                title: 'JS',
+                number: '9'
+            },
+        ],
+    }
+
+    const [selectInfos, setSelectInfos] = useState('framework')
+    const [listSkillState, setListSkillState] = useState(listSkill['framework'])
+
+    useEffect(() => {
+
+        setListSkillState(listSkill[selectInfos])
+        
+    }, [selectInfos])
+
     return (
-        <div>
-            
-        </div>
+        <section id="skill" className={styles.skill}>
+            <h2>SKILL</h2>
+            <div className={styles.skillBody}>
+                <div>
+                    <button onClick={() => setSelectInfos('framework')} className={selectInfos === 'framework' ? styles.active : ''}>Framework</button>
+                    <button onClick={() => setSelectInfos('languages')} className={selectInfos === 'languages' ? styles.active : ''}>Languages</button>
+                </div>
+                <div>
+                    {
+                        listSkillState.map((x) => {
+                            return (
+                                <div>
+                                    <h3>{x.title}</h3>
+                                    <hr />
+                                    <div>
+
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import About from '../composants/home/About'
 import Contact from '../composants/home/Contact'
 import Feedback from '../composants/home/Feedback'
 import Services from '../composants/home/Services'
+import Skill from '../composants/home/Skill'
 import Navbar from '../composants/Navbar'
 import Particle from '../composants/Particle'
 import styles from '../styles/Home.module.scss'
@@ -20,6 +21,8 @@ export default function Home() {
                 <About />
 
                 <Services />
+
+                <Skill />
 
                 <Feedback />
 
