0cb1c8254e599b35546560286331086fb7235644
Edofo <Edofo@users.noreply.github.com>
2022-02-07 16:43:44 +0100

    Update Portfolio.js

--- composants/home/Portfolio.js
+++ composants/home/Portfolio.js
@@ -1,7 +1,12 @@
+import styles from '../../styles/home/Portfolio.module.scss'
+
 const Portfolio = () => {
     return (
-        <section id="portfolio">
-            
+        <section id="portfolio" className={styles.portfolio}>
+            <h2>PORTFOLIO</h2>
+            <div className={styles.portfolioBody}>
+
+            </div>
         </section>
     )
 }
