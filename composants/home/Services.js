20a12e079b0e6a0bafac51e7280376bffa76821c
Edofo <48387251+Edofo@users.noreply.github.com>
2022-02-16 15:12:26 +0100

    Update Services.js

--- composants/home/Services.js
+++ composants/home/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
                 <div className={styles.card}>
                     <i className="fad fa-alarm-clock"></i>
                     <h3>Planning</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p>Vous vous demandez surement quelles sont mes horraires ? C'est très simple, étant donné que je suis en cours la journée je peux travailler uniquement le soir et le week-end.</p>
                 </div>
                 <div className={styles.card}>
                     <i className="fab fa-react"></i>
@@ -18,7 +18,7 @@ const Services = () => {
                 <div className={styles.card}>
                     <i className="fad fa-pencil-ruler"></i>
                     <h3>UI/UX Design</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p>Vous avez surement déjà pu apercevoir des sites pas très pratiques ou pas très jolies. Mais avec moi je peux vous assurer que chaque site que je réalise est 100% facile d'utilisation et jolie !</p>
                 </div>
             </div>
         </section>
